dcee405705891dc10a832cc23addcd6e3415a37f
witcradg <witcradg@gmail.com>
2021-03-12 12:46:54 -0800

    Refactor out Table and Column Definitions for Full Width Resizable

--- src/components/full-width-resizable-table/index.js
+++ src/components/full-width-resizable-table/index.js
@@ -1,284 +1,135 @@
-import React from 'react'
-import styled from 'styled-components'
-import {
-  useTable,
-  useResizeColumns,
-  useFlexLayout,
-  useRowSelect,
-} from 'react-table'
-import Layout from '../layout';
+import React from 'react';
+import styled from 'styled-components';
 
-import makeData from '../../util/makeData'
+import Layout from '../layout';
+import getColumnDefinitions from './columnDefinitions';
+
+import makeData from '../../util/makeData';
+import { Table } from './Table';
 
 const Styles = styled.div`
-  padding: 1rem;
-  ${'' /* These styles are suggested for the table fill all available space in its containing element */}
-  display: block;
-  ${'' /* These styles are required for a horizontaly scrollable table overflow */}
-  overflow: auto;
+	padding: 1rem;
+	${'' /* These styles are suggested for the table fill all available space in its containing element */} display: block;
+	${'' /* These styles are required for a horizontaly scrollable table overflow */} overflow: auto;
 
-  .table {
-    border-spacing: 0;
-    border: 1px solid black;
+	.table {
+		border-spacing: 0;
+		border: 1px solid black;
 
-    .thead {
-      ${'' /* These styles are required for a scrollable body to align with the header properly */}
-      overflow-y: auto;
-      overflow-x: hidden;
-    }
+		.thead {
+			${'' /* These styles are required for a scrollable body to align with the header properly */} overflow-y: auto;
+			overflow-x: hidden;
+		}
 
-    .tbody {
-      ${'' /* These styles are required for a scrollable table body */}
-      overflow-y: scroll;
-      overflow-x: hidden;
-      height: 250px;
-    }
+		.tbody {
+			${'' /* These styles are required for a scrollable table body */} overflow-y: scroll;
+			overflow-x: hidden;
+			height: 250px;
+		}
 
-    .tr {
-      :last-child {
-        .td {
-          border-bottom: 0;
-        }
-      }
-      border-bottom: 1px solid black;
-    }
+		.tr {
+			:last-child {
+				.td {
+					border-bottom: 0;
+				}
+			}
+			border-bottom: 1px solid black;
+		}
 
-    .th,
-    .td {
-      margin: 0;
-      padding: 0.5rem;
-      border-right: 1px solid black;
+		.th,
+		.td {
+			margin: 0;
+			padding: 0.5rem;
+			border-right: 1px solid black;
 
-      ${'' /* In this example we use an absolutely position resizer,
-       so this is required. */}
-      position: relative;
+			${'' /* In this example we use an absolutely position resizer,
+       so this is required. */} position: relative;
 
-      :last-child {
-        border-right: 0;
-      }
+			:last-child {
+				border-right: 0;
+			}
 
-      .resizer {
-        right: 0;
-        background: blue;
-        width: 10px;
-        height: 100%;
-        position: absolute;
-        top: 0;
-        z-index: 1;
-        ${'' /* prevents from scrolling while dragging on touch devices */}
-        touch-action :none;
+			.resizer {
+				right: 0;
+				background: blue;
+				width: 10px;
+				height: 100%;
+				position: absolute;
+				top: 0;
+				z-index: 1;
+				${'' /* prevents from scrolling while dragging on touch devices */} touch-action :none;
 
-        &.isResizing {
-          background: red;
-        }
-      }
-    }
-  }
-`
+				&.isResizing {
+					background: red;
+				}
+			}
+		}
+	}
+`;
 
-const headerProps = (props, { column }) => getStyles(props, column.align)
+export const headerProps = (props, { column }) => getStyles(props, column.align);
 
-const cellProps = (props, { cell }) => getStyles(props, cell.column.align)
+export const cellProps = (props, { cell }) => getStyles(props, cell.column.align);
 
 const getStyles = (props, align = 'left') => [
-  props,
-  {
-    style: {
-      justifyContent: align === 'right' ? 'flex-end' : 'flex-start',
-      alignItems: 'flex-start',
-      display: 'flex',
-    },
-  },
-]
+	props,
+	{
+		style: {
+			justifyContent: align === 'right' ? 'flex-end' : 'flex-start',
+			alignItems: 'flex-start',
+			display: 'flex'
+		}
+	}
+];
 
-const IndeterminateCheckbox = React.forwardRef(
-  ({ indeterminate, ...rest }, ref) => {
-    const defaultRef = React.useRef()
-    const resolvedRef = ref || defaultRef
+export const IndeterminateCheckbox = React.forwardRef(({ indeterminate, ...rest }, ref) => {
+	const defaultRef = React.useRef();
+	const resolvedRef = ref || defaultRef;
 
-    React.useEffect(() => {
-      resolvedRef.current.indeterminate = indeterminate
-    }, [resolvedRef, indeterminate])
+	React.useEffect(
+		() => {
+			resolvedRef.current.indeterminate = indeterminate;
+		},
+		[ resolvedRef, indeterminate ]
+	);
 
-    return (
-      <>
-        <input type="checkbox" ref={resolvedRef} {...rest} />
-      </>
-    )
-  }
-)
+	return (
+		<React.Fragment>
+			<input type="checkbox" ref={resolvedRef} {...rest} />
+		</React.Fragment>
+	);
+});
 
-function Table({ columns, data }) {
-  const defaultColumn = React.useMemo(
-    () => ({
-      // When using the useFlexLayout:
-      minWidth: 30, // minWidth is only used as a limit for resizing
-      width: 150, // width is used for both the flex-basis and flex-grow
-      maxWidth: 200, // maxWidth is only used as a limit for resizing
-    }),
-    []
-  )
+export default function FullWidthResizableTable() {
+	const columns = React.useMemo(() => getColumnDefinitions(), []);
 
-  const { getTableProps, headerGroups, rows, prepareRow } = useTable(
-    {
-      columns,
-      data,
-      defaultColumn,
-    },
-    useResizeColumns,
-    useFlexLayout,
-    useRowSelect,
-    hooks => {
-      hooks.allColumns.push(columns => [
-        // Let's make a column for selection
-        {
-          id: 'selection',
-          disableResizing: true,
-          minWidth: 35,
-          width: 35,
-          maxWidth: 35,
-          // The header can use the table's getToggleAllRowsSelectedProps method
-          // to render a checkbox
-          Header: ({ getToggleAllRowsSelectedProps }) => (
-            <div>
-              <IndeterminateCheckbox {...getToggleAllRowsSelectedProps()} />
-            </div>
-          ),
-          // The cell can use the individual row's getToggleRowSelectedProps method
-          // to the render a checkbox
-          Cell: ({ row }) => (
-            <div>
-              <IndeterminateCheckbox {...row.getToggleRowSelectedProps()} />
-            </div>
-          ),
-        },
-        ...columns,
-      ])
-      hooks.useInstanceBeforeDimensions.push(({ headerGroups }) => {
-        // fix the parent group of the selection button to not be resizable
-        const selectionGroupHeader = headerGroups[0].headers[0]
-        selectionGroupHeader.canResize = false
-      })
-    }
-  )
+	const data = React.useMemo(() => makeData(20), []);
 
-  return (
-    <div {...getTableProps()} className="table">
-      <div>
-        {headerGroups.map(headerGroup => (
-          <div
-            {...headerGroup.getHeaderGroupProps({
-              // style: { paddingRight: '15px' },
-            })}
-            className="tr"
-          >
-            {headerGroup.headers.map(column => (
-              <div {...column.getHeaderProps(headerProps)} className="th">
-                {column.render('Header')}
-                {/* Use column.getResizerProps to hook up the events correctly */}
-                {column.canResize && (
-                  <div
-                    {...column.getResizerProps()}
-                    className={`resizer ${
-                      column.isResizing ? 'isResizing' : ''
-                    }`}
-                  />
-                )}
-              </div>
-            ))}
-          </div>
-        ))}
-      </div>
-      <div className="tbody">
-        {rows.map(row => {
-          prepareRow(row)
-          return (
-            <div {...row.getRowProps()} className="tr">
-              {row.cells.map(cell => {
-                return (
-                  <div {...cell.getCellProps(cellProps)} className="td">
-                    {cell.render('Cell')}
-                  </div>
-                )
-              })}
-            </div>
-          )
-        })}
-      </div>
-    </div>
-  )
+	return (
+		<React.Fragment>
+			<Layout title="React Table: Full Width Resizable">
+				<div>
+					<p className="p-0">
+						<a
+							className="anchor"
+							href="https://github.com/tannerlinsley/react-table/tree/master/examples/full-width-resizable-table/src"
+						>
+							Repository Source
+						</a>
+					</p>
+					<p>
+						<a
+							className="anchor"
+							href="https://codesandbox.io/s/github/tannerlinsley/react-table/tree/master/examples/full-width-resizable-table"
+						>
+							Source code in codesandbox
+						</a>
+					</p>
+				</div>
+			</Layout>
+			<Styles>
+				<Table columns={columns} data={data} />
+			</Styles>
+		</React.Fragment>
+	);
 }
-
-function FullWidthResizableTable() {
-  const columns = React.useMemo(
-    () => [
-      {
-        Header: 'Name',
-        columns: [
-          {
-            Header: 'First Name',
-            accessor: 'firstName',
-          },
-          {
-            Header: 'Last Name',
-            accessor: 'lastName',
-          },
-        ],
-      },
-      {
-        Header: 'Info',
-        columns: [
-          {
-            Header: 'Age',
-            accessor: 'age',
-            width: 50,
-            align: 'right',
-          },
-          {
-            Header: 'Visits',
-            accessor: 'visits',
-            width: 50,
-            align: 'right',
-          },
-          {
-            Header: 'Status',
-            accessor: 'status',
-          },
-          {
-            Header: 'Profile Progress',
-            accessor: 'progress',
-          },
-        ],
-      },
-    ],
-    []
-  )
-
-  const data = React.useMemo(() => makeData(20), [])
-
-  return (
-    <Layout title="React Table: Full Width Resizable">
-        <p className="p-0">
-            <a
-                className="anchor"
-                href="https://github.com/tannerlinsley/react-table/tree/master/examples/full-width-resizable-table/src"
-            >
-                Repository Source
-            </a>
-        </p>
-        <p>
-            <a
-                className="anchor"
-                href="https://codesandbox.io/s/github/tannerlinsley/react-table/tree/master/examples/full-width-resizable-table"
-            >
-                Source code in codesandbox
-            </a>
-        </p>
-        <Styles>
-            <Table columns={columns} data={data} />
-        </Styles>
-    </Layout>
-  )
-}
-
-export default FullWidthResizableTable
